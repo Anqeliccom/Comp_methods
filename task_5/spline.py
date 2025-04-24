a6a25f9c83f9b8fda6e5314a9f5fbea066495d5b
Anqeliccom <hamidulinaanzelika462@gmail.com>
2025-04-24 20:07:46 +0700

    spline and tridiagonal method added

---
+++ task_5/spline.py
@@ -0,0 +1,99 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import task_5
+
+def func(x):
+    return 1 / (1 + 25 * x ** 2)
+
+def get_coefficients(x, y, lcond, rcond):
+    n = len(x) - 1
+    h = [x[i + 1] - x[i] for i in range(n)]
+
+    A = []
+    B = []
+    C = []
+    rhs = []
+
+    for i in range(1, n):
+        A.append(h[i - 1])
+        C.append(2 * (h[i - 1] + h[i]))
+        B.append(h[i])
+        rhs.append(3 * ((y[i + 1] - y[i]) / h[i] - (y[i] - y[i - 1]) / h[i - 1]))
+
+    if lcond[0]:  # первая производная
+        A = [0.0] + A
+        C = [2 * h[0]] + C
+        B = [h[0]] + B
+        rhs = [3 * ((y[1] - y[0]) / h[0] - lcond[1])] + rhs
+    else:  # вторая производная
+        A = [0.0] + A
+        C = [1.0] + C
+        B = [0.0] + B
+        rhs = [lcond[1]] + rhs
+
+    if rcond[0]:
+        A = A + [2 * h[n - 1]]
+        C = C + [h[n - 1]]
+        B = B + [0.0]
+        rhs = rhs + [3 * ((y[n] - y[n - 1]) / h[n - 1] - rcond[1])]
+    else:
+        A = A + [0.0]
+        C = C + [1.0]
+        B = B + [0.0]
+        rhs = rhs + [rcond[1]]
+
+    c = task_5.tridiagonal_method(A, C, B, rhs)
+
+    b = [(y[i + 1] - y[i]) / h[i] - h[i] * (2 * c[i] + c[i + 1]) / 3 for i in range(n)]
+    d = [(c[i + 1] - c[i]) / (3 * h[i]) for i in range(n)]
+
+    return c, b, d
+
+def get_spline(x, y, c, b, d, xp):
+    for i in range(len(x) - 1):
+        if x[i] <= xp <= x[i + 1]:
+            dx = xp - x[i]
+            return y[i] + b[i] * dx + c[i] * dx ** 2 + d[i] * dx ** 3
+    return None
+
+def plot_spline_vs_function(f, a, b, n, lcond, rcond):
+    x = np.linspace(a, b, n + 1)
+    y = f(x)
+    c, b_coef, d = get_coefficients(x, y, lcond, rcond)
+    xp = np.linspace(a, b, 500)
+    sp = [get_spline(x, y, c, b_coef, d, xi) for xi in xp]
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(xp, f(xp), label='f(x)')
+    plt.plot(xp, sp, '--', label='Spline')
+    plt.legend()
+    plt.title('Сравнение функции Рунге и кубического сплайна')
+    plt.grid()
+    plt.show()
+
+def plot_max_errors(f, a, b, lcond, rcond, max_nodes):
+    ns = range(10, max_nodes + 1, 10)
+    max_errors = []
+
+    for n in ns:
+        x = np.linspace(a, b, n + 1)
+        y = f(x)
+        c, b_coef, d = get_coefficients(x, y, lcond, rcond)
+        xp = np.linspace(a, b, 500)
+        sp = [get_spline(x, y, c, b_coef, d, xi) for xi in xp]
+        max_errors.append(max(abs(si - fxi) for si, fxi in zip(sp, f(xp))))
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(ns, max_errors, 'o-')
+    plt.xlabel('Количество узлов')
+    plt.ylabel('Максимальная погрешность')
+    plt.title('Зависимость погрешности от количества узлов')
+    plt.grid()
+    plt.show()
+
+a, b = -1, 1
+lcond = (False, 0.0)  # S''(a) = 0
+rcond = (False, 0.0)  # S''(b) = 0
+
+plot_spline_vs_function(func, a, b, 50, lcond, rcond)
+plot_max_errors(func, a, b, lcond, rcond, 100)
---
+++ task_5/task_5.py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def equation_rhs(x):
+    return np.cos(x)
+
+def tridiagonal_method(A, C, B, F):
+    k1, m1 = B[0], F[0]
+    k2, m2 = A[-1], F[-1]
+    alpha, beta = [k1], [m1]
+    n = len(F)
+    for i in range(n - 1):
+        alpha.append(B[i] / (-C[i] - A[i] * alpha[i]))
+        beta.append((A[i] * beta[i] + F[i]) / (-C[i] - A[i] * alpha[i]))
+
+    y = [0.0] * n
+    y[-1] = (m2 + k2 * beta[n - 1]) / (1 - k2 * alpha[n - 1])
+    for i in range(n - 1, 0, -1):
+        y[i - 1] = alpha[i] * y[i] + beta[i]
+    return y
+
+def get_matrices(func, lcond, rcond, a, b, n):
+    h = (b - a) / n
+    X = [a + i * h for i in range(1,n)]
+
+    A = [1 / h ** 2] * (n)
+    C = [-2 / h ** 2] * (n)
+    B = [1 / h ** 2] * (n)
+    F = [-func(x) for x in X]
+    X = [a] + X + [b]
+
+    if lcond[0]:  # производная
+        A = [0.0] + A
+        C = [-1 / h] + C
+        B = [1 / h] + B
+        F = [lcond[1]] + F
+    else:  # функция
+        A = [0.0] + A
+        C = [1.0] + C
+        B = [0.0] + B
+        F = [lcond[1]] + F
+
+    if rcond[0]:
+        A = A + [-1 / h]
+        C = C + [1 / h]
+        B = B + [0.0]
+        F = F + [rcond[1]]
+    else:
+        A = A + [0.0]
+        C = C + [1.0]
+        B = B + [0.0]
+        F = F + [rcond[1]]
+
+    return A, C, B, F, X
+
+
+def examples(example_num):
+    ex = {
+        1: ((False, 0.0), (False, 1.0), lambda x: -np.cos(x) + x / np.pi + 1 / 2), # y(a)=0, y(b)=1
+        2: ((True, 0.0), (False, 1.0), lambda x: -np.cos(x) + x - np.pi / 2 + 1), # y'(a)=0, y(b)=1
+        3: ((False, 0.0), (True, 0.0), lambda x: -np.cos(x) - x - np.pi / 2), # y(a)=0, y'(b)=0
+    }
+    return ex.get(example_num, ex[1])
+
+def plot_max_errors(a, b, lcond, rcond, exact_sol, max_nodes):
+    ns = range(10, max_nodes + 1, 10)
+    max_errors = []
+
+    for n in ns:
+        A, C, B, F, X = get_matrices(equation_rhs, lcond, rcond, a, b, n)
+        y_num = tridiagonal_method(A, C, B, F)
+        y_exact = [exact_sol(x) for x in X]
+        max_errors.append(max(abs(num - exact) for num, exact in zip(y_num, y_exact)))
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(ns, max_errors, 'o-')
+    plt.xlabel('Количество узлов')
+    plt.ylabel('Максимальная погрешность')
+    plt.title('Зависимость погрешности от количества узлов')
+    plt.grid(True)
+    plt.show()
+
+if __name__ == "__main__":
+    a, b = -np.pi / 2, np.pi / 2
+    lcond, rcond, exact_solution = examples(3)
+
+    n = 100
+    A, C, B, F, X = get_matrices(equation_rhs, lcond, rcond, a, b, n)
+    y_num = tridiagonal_method(A, C, B, F)
+    y_exact = [exact_solution(x) for x in X]
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(X, y_num, label="Численное решение", marker='o', markersize=3, linestyle='-')
+    plt.plot(X, y_exact, label="Точное решение")
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Сравнение численного и точного решений')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+    plot_max_errors(a, b, lcond, rcond, exact_solution, 100)
